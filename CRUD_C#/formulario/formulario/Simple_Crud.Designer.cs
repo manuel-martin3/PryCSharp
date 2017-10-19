b4294cb78a6a3c2c5555fdfed59dc9b7b7451263
manuel-martin3 <manuel_martin3@hotmail.com>
2017-10-19 16:29:11 -0500

    simple CRUD
    Codigo CRUD actualizado

--- CRUD_C#/formulario/formulario/Simple_Crud.Designer.cs
+++ CRUD_C#/formulario/formulario/Simple_Crud.Designer.cs
@@ -35,23 +35,27 @@
             this.btn_insert = new System.Windows.Forms.Button();
             this.btn_delete = new System.Windows.Forms.Button();
             this.btn_search = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_nom = new System.Windows.Forms.TextBox();
+            this.txt_ape = new System.Windows.Forms.TextBox();
+            this.txt_dni = new System.Windows.Forms.TextBox();
+            this.cmb_data = new System.Windows.Forms.ComboBox();
+            this.dgv_detalle = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_detalle)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_cancel
             // 
-            this.btn_cancel.Location = new System.Drawing.Point(36, 324);
+            this.btn_cancel.Location = new System.Drawing.Point(6, 14);
             this.btn_cancel.Name = "btn_cancel";
             this.btn_cancel.Size = new System.Drawing.Size(75, 23);
             this.btn_cancel.TabIndex = 0;
@@ -60,7 +64,7 @@
             // 
             // btn_new
             // 
-            this.btn_new.Location = new System.Drawing.Point(117, 324);
+            this.btn_new.Location = new System.Drawing.Point(87, 14);
             this.btn_new.Name = "btn_new";
             this.btn_new.Size = new System.Drawing.Size(75, 23);
             this.btn_new.TabIndex = 0;
@@ -69,7 +73,7 @@
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(198, 324);
+            this.btn_exit.Location = new System.Drawing.Point(168, 14);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(75, 23);
             this.btn_exit.TabIndex = 0;
@@ -78,83 +82,88 @@
             // 
             // btn_update
             // 
-            this.btn_update.Location = new System.Drawing.Point(428, 270);
+            this.btn_update.Location = new System.Drawing.Point(6, 105);
             this.btn_update.Name = "btn_update";
             this.btn_update.Size = new System.Drawing.Size(75, 23);
             this.btn_update.TabIndex = 0;
             this.btn_update.Text = "Actualizar";
             this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
             // 
             // btn_insert
             // 
-            this.btn_insert.Location = new System.Drawing.Point(428, 241);
+            this.btn_insert.Location = new System.Drawing.Point(6, 76);
             this.btn_insert.Name = "btn_insert";
             this.btn_insert.Size = new System.Drawing.Size(75, 23);
             this.btn_insert.TabIndex = 0;
             this.btn_insert.Text = "Ingresar";
             this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
             // 
             // btn_delete
             // 
-            this.btn_delete.Location = new System.Drawing.Point(428, 212);
+            this.btn_delete.Location = new System.Drawing.Point(6, 47);
             this.btn_delete.Name = "btn_delete";
             this.btn_delete.Size = new System.Drawing.Size(75, 23);
             this.btn_delete.TabIndex = 0;
             this.btn_delete.Text = "Eliminar";
             this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(428, 183);
+            this.btn_search.Location = new System.Drawing.Point(6, 18);
             this.btn_search.Name = "btn_search";
             this.btn_search.Size = new System.Drawing.Size(75, 23);
             this.btn_search.TabIndex = 0;
             this.btn_search.Text = "Buscar";
             this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
-            // textBox1
+            // txt_id
             // 
-            this.textBox1.Location = new System.Drawing.Point(92, 79);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 1;
+            this.txt_id.Location = new System.Drawing.Point(92, 79);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(121, 20);
+            this.txt_id.TabIndex = 1;
             // 
-            // textBox2
+            // txt_nom
             // 
-            this.textBox2.Location = new System.Drawing.Point(92, 105);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 1;
+            this.txt_nom.Location = new System.Drawing.Point(92, 105);
+            this.txt_nom.Name = "txt_nom";
+            this.txt_nom.Size = new System.Drawing.Size(121, 20);
+            this.txt_nom.TabIndex = 1;
             // 
-            // textBox3
+            // txt_ape
             // 
-            this.textBox3.Location = new System.Drawing.Point(294, 79);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 20);
-            this.textBox3.TabIndex = 1;
+            this.txt_ape.Location = new System.Drawing.Point(294, 79);
+            this.txt_ape.Name = "txt_ape";
+            this.txt_ape.Size = new System.Drawing.Size(121, 20);
+            this.txt_ape.TabIndex = 1;
             // 
-            // textBox4
+            // txt_dni
             // 
-            this.textBox4.Location = new System.Drawing.Point(294, 105);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 20);
-            this.textBox4.TabIndex = 1;
+            this.txt_dni.Location = new System.Drawing.Point(294, 105);
+            this.txt_dni.Name = "txt_dni";
+            this.txt_dni.Size = new System.Drawing.Size(121, 20);
+            this.txt_dni.TabIndex = 1;
             // 
-            // comboBox1
+            // cmb_data
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(92, 133);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.cmb_data.FormattingEnabled = true;
+            this.cmb_data.Location = new System.Drawing.Point(92, 133);
+            this.cmb_data.Name = "cmb_data";
+            this.cmb_data.Size = new System.Drawing.Size(121, 21);
+            this.cmb_data.TabIndex = 2;
             // 
-            // dataGridView1
+            // dgv_detalle
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(36, 160);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(379, 150);
-            this.dataGridView1.TabIndex = 3;
+            this.dgv_detalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_detalle.Location = new System.Drawing.Point(36, 160);
+            this.dgv_detalle.Name = "dgv_detalle";
+            this.dgv_detalle.Size = new System.Drawing.Size(379, 150);
+            this.dgv_detalle.TabIndex = 3;
+            this.dgv_detalle.DoubleClick += new System.EventHandler(this.dgv_detalle_DoubleClick);
             // 
             // label1
             // 
@@ -202,33 +211,53 @@
             this.label5.TabIndex = 5;
             this.label5.Text = "Ficha de Alumnos";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_search);
+            this.groupBox1.Controls.Add(this.btn_delete);
+            this.groupBox1.Controls.Add(this.btn_insert);
+            this.groupBox1.Controls.Add(this.btn_update);
+            this.groupBox1.Location = new System.Drawing.Point(421, 153);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(88, 158);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_exit);
+            this.groupBox2.Controls.Add(this.btn_cancel);
+            this.groupBox2.Controls.Add(this.btn_new);
+            this.groupBox2.Location = new System.Drawing.Point(36, 309);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(473, 43);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
             // SimpleCRUD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 372);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btn_search);
-            this.Controls.Add(this.btn_delete);
-            this.Controls.Add(this.btn_insert);
-            this.Controls.Add(this.btn_update);
-            this.Controls.Add(this.btn_exit);
-            this.Controls.Add(this.btn_new);
-            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.dgv_detalle);
+            this.Controls.Add(this.cmb_data);
+            this.Controls.Add(this.txt_dni);
+            this.Controls.Add(this.txt_nom);
+            this.Controls.Add(this.txt_ape);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.groupBox2);
             this.Name = "SimpleCRUD";
             this.Text = "Simple_CRUD";
             this.Load += new System.EventHandler(this.SimpleCRUD_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_detalle)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,17 +272,19 @@
         private System.Windows.Forms.Button btn_insert;
         private System.Windows.Forms.Button btn_delete;
         private System.Windows.Forms.Button btn_search;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_nom;
+        private System.Windows.Forms.TextBox txt_ape;
+        private System.Windows.Forms.TextBox txt_dni;
+        private System.Windows.Forms.ComboBox cmb_data;
+        private System.Windows.Forms.DataGridView dgv_detalle;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
