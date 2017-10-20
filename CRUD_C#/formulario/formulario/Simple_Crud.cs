5af8bf9e1b0c8664356a5f88002ea87b5f9432f6
manuel-martin3 <manuel_martin3@hotmail.com>
2017-10-19 21:03:36 -0500

    Simple CRUD VBasic
    Simple CRUD ejemplo realizado con VBasic

--- CRUD_C#/formulario/formulario/Simple_Crud.cs
+++ CRUD_C#/formulario/formulario/Simple_Crud.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using logica;
+using entidad;
 
 namespace formulario
 {
@@ -19,7 +21,116 @@ namespace formulario
 
         private void SimpleCRUD_Load(object sender, EventArgs e)
         {
+            this.refresca();
+            this.cargarcombo();
+        }
 
-        }              
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            string rpta = "";
+            try
+            {
+                lgk_alu obj = new lgk_alu();
+                propiedades enti = new propiedades();
+                enti.nomalu = txt_nom.Text;
+                enti.apealu = txt_ape.Text;
+                enti.dnialu = txt_dni.Text;
+                obj.regi_alu(enti);
+                this.Refresh();
+                cargarcombo();
+                rpta = "Buen trabajo...";                
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;                
+            }
+
+            MessageBox.Show(rpta);
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            string rpta = "";
+            try
+            {
+                lgk_alu obj = new lgk_alu();
+                propiedades enti = new propiedades();
+                enti.idalu = Int16.Parse(this.txt_id.Text);                
+                enti.nomalu = txt_nom.Text;
+                enti.apealu = txt_ape.Text;
+                enti.dnialu = txt_dni.Text;
+                obj.regi_alu(enti);
+                this.Refresh();
+                cargarcombo();
+                rpta = "Buen trabajo...";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+
+            MessageBox.Show(rpta);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            string rpta = "";
+            try
+            {
+                lgk_alu obj = new lgk_alu();
+                propiedades enti = new propiedades();
+                enti.idalu = Int16.Parse(this.txt_id.Text);                
+                obj.eli_alu(enti);
+                this.Refresh();
+                cargarcombo();
+                rpta = "Buen trabajo...";                
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;                
+            }
+
+            MessageBox.Show(rpta);
+        }
+
+        private void dgv_detalle_DoubleClick(object sender, EventArgs e)
+        {
+            this.txt_id.Text = Convert.ToString(this.dgv_detalle.CurrentRow.Cells["idalu"].Value);
+            this.txt_nom.Text = Convert.ToString(this.dgv_detalle.CurrentRow.Cells["nomalu"].Value);
+            this.txt_ape.Text = Convert.ToString(this.dgv_detalle.CurrentRow.Cells["apealu"].Value);
+            this.txt_dni.Text = Convert.ToString(this.dgv_detalle.CurrentRow.Cells["dnialu"].Value);
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            this.buscar();
+        }
+
+        #region metodos
+        public void buscar()
+        {
+            int a = Int16.Parse(this.txt_id.Text);
+            lgk_alu obj = new lgk_alu();
+            propiedades enti = new propiedades();
+            enti.idalu = a;
+            dgv_detalle.DataSource = obj.busca_alu(enti);
+        }
+
+        public void refresca() {
+            lgk_alu obj = new lgk_alu();
+            propiedades enti = new propiedades();
+            dgv_detalle.DataSource = obj.lista_alu();
+        }
+
+        public void cargarcombo() {
+            lgk_alu obj = new lgk_alu();
+            propiedades enti = new propiedades();
+            cmb_data.DataSource = obj.lista_alu();
+            cmb_data.DisplayMember = "nomalu";
+            cmb_data.ValueMember = "idalu";
+        }
+
+        #endregion
+                
     }
 }
