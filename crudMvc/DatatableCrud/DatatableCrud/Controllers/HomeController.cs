9aab2ec2c4f963eba4a47a128e01f907a67bd128
manuel-martin3 <manuel_martin3@hotmail.com>
2017-10-27 19:38:54 -0500

    MVC Crud C#
    Simple Proyecto CRUD en MVC con C#

---
+++ crudMvc/DatatableCrud/DatatableCrud/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatatableCrud.Models;
+
+namespace DatatableCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees() 
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities()) 
+            {
+                var employees = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        #region Guardar....Editar....Eliminar.... 
+                
+        [HttpGet]
+        public ActionResult save(int id) 
+        { 
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeId == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult save(Employee emp) 
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    if (emp.EmployeeId > 0)
+	                {
+		                //Edita
+                        var v = dc.Employees.Where(a => a.EmployeeId == emp.EmployeeId).FirstOrDefault();
+                        if (v != null)
+	                    {
+		                    v.FirstName = emp.FirstName;
+                            v.LastName = emp.LastName;
+                            v.EmailId = emp.EmailId;
+                            v.City = emp.City;
+                            v.Country = emp.Country;
+	                    }
+
+	                }else
+	                {
+                        //Guardar
+                        dc.Employees.Add(emp);
+	                }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult{ Data =  new { status = status} };
+        }
+        
+        
+        // Eliminar....
+        [HttpGet]
+        public ActionResult Delete(int id) 
+        {         
+            using(MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeId == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id) 
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities()) 
+            {
+                var v = dc.Employees.Where(a => a.EmployeeId == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Employees.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ crudMvc/DatatableCrud/DatatableCrud/Models/Extended/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatatableCrud.Models
+{
+    [MetadataType(typeof(EmployMetadata))]
+    public partial class Employee
+    {
+
+    }
+    
+    public class EmployMetadata
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingresar Nombre..." )]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingresar Apellidos...")]
+        public string LastName { get; set; }
+    }
+}
